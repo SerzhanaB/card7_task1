83c41eeda14a4f24183d34d90fe2cb5031a6e3c8
Serzhana <buraevase@gmail.com>
2021-09-12 12:06:59 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Products extends Component {
+state ={
+  products:[]}
+
+componentDidMount() {
+this.setState({
+
+products: [
+{id:1, name:'Товар 1', price:1200},
+{id:2, name:'Товар 2', price:1500},
+{id:3, name:'Товар 3', price:1700},
+{id:4, name:'Товар 4', price:1100},
+{id:5, name:'Товар 5', price:1300},
+{id:6, name:'Товар 6', price:1300},]
+
+})
+
 }
 
-export default App;
+delete = (targetId) => this.setState({ products: this.state.products.filter(({id})=> id!==targetId)})
+
+create = (id,name,price) => this.setState({ products: [...this.state.products, {id:id,name:name, price:price}]})
+
+update = (targetId,name,price) => this.setState({ products: this.state.products.map(product=>(
+product.id!== targetId ? product:{id: targetId,name: name,price:price}
+
+
+))})
+
+
+
+  render(){
+    const { products}=this.state
+  
+
+return (
+<table className="products">
+
+  <thead>
+  <tr>
+    <th className="products_th">№</th>
+    <th className="products_th">Название</th>
+    <th className="products_th">Цена</th>
+    <th className="products_th">Действие</th>
+    </tr>
+     </thead>
+<tbody>
+{products.map( ({id,name,price})=> (
+
+  <tr key={id}>
+
+    <td className="products_td">{id}</td>
+    <td className="products_td">{name}</td>
+    <td className="products_td">{price}</td>
+    <td className="products_td"> 
+    <button className="products_btn" onClick={()=>{this.delete(id)}} > Удалить</button>
+    </td>
+    </tr>
+)
+)}
+  </tbody>
+  </table>
+  );
+}
+}
+
+export default Products;
